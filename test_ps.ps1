af62a2f2d1446c885ca7de06109e46aa8d6b63bb
Kumari Puja <christikiyoon27@gmail.com>
2026-06-06 23:49:19 +0530

    chore: Remove temporary compression test files

--- test_ps.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("c:\Users\himan\Downloads\Portfolio\Portfolio_media\render-livingroom.jpg")
-$bmp = New-Object System.Drawing.Bitmap($img, 800, ([int]($img.Height * (800.0 / $img.Width))))
-$codec = [System.Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | Where-Object { $_.MimeType -eq 'image/jpeg' }
-$encoderParams = New-Object System.Drawing.Imaging.EncoderParameters(1)
-$encoderParams.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter([System.Drawing.Imaging.Encoder]::Quality, [long]70)
-$bmp.Save("c:\Users\himan\Downloads\Portfolio\Portfolio_media\test_compress.jpg", $codec, $encoderParams)
-$bmp.Dispose()
-$img.Dispose()
